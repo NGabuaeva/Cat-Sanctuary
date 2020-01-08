0aa7e0469789672a990cfe0df03e9cacc697e142
Natalia Gabuaeva <ngabuaeva@gmail.com>
2020-01-08 15:28:26 -0500

    add a cat

---
+++ app/components/AddForm.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { addCat } from '../reducers/cats'
+import { connect } from 'react-redux'
+
+
+//Form to add a cat
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      breed: '',
+      age: undefined,
+      hobby: '',
+      favoriteSnack: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  //update state as we're typing in one of the fields
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  //add a new cat to the database by calling the thunk on submit
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.addCat(this.state)
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className='form'>
+        <label htmlFor='name'>Name</label>
+        <input onChange={this.handleChange} type='text' name='name' value={this.state.name} />
+
+        <label htmlFor='age'>Age</label>
+        <input onChange={this.handleChange} type='text' name='age' value={this.state.age} />
+
+        <label htmlFor='hobby'>Hobby</label>
+        <input onChange={this.handleChange} type='text' name='hobby' value={this.state.hobby} />
+
+        <label htmlFor='favoriteSnack'>Favorite Snack</label>
+        <input onChange={this.handleChange} type='text' name='favoriteSnack' value={this.state.favoriteSnack} />
+
+        <label htmlFor='breed'>Breed</label>
+        <input onChange={this.handleChange} type='text' name='breed' value={this.state.breed} />
+
+        <button type='submit'>Submit</button>
+      </form>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addCat: (cat) => dispatch(addCat(cat))
+})
+export default connect(null, mapDispatchToProps)(Form)
--- app/components/Cats.js
+++ app/components/Cats.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux"
 import { getCats } from '../reducers/cats'
 import { Link } from 'react-router-dom'
-
+import AddForm from './AddForm'
 
 //component to list all the cats
 function Cats() {
   const cats = useSelector(state => state.cats)
   const dispatch = useDispatch()
+  const [form, setForm] = useState('')
 
   //fetch all cats from the database
   useEffect(() => { dispatch(getCats()) }, [])
@@ -15,12 +16,14 @@ function Cats() {
     <div className='catList'>
 
       {/* map through the cats array to create a container for each one */}
-      {cats.map(cat => (<div className='singleCat' key={cat.id}>
+      {cats.map(cat => (<div key={cat.id}>
         <Link to={`/cats/${cat.id}`}>
           <img className='catImg' src={cat.imageUrl} />
           <h3 className='catName'>{cat.name}</h3>
         </Link>
       </div>))}
+      <button onClick={() => !form ? setForm(<AddForm />) : setForm('')} className='button'>ADD A CAT</button>
+      {form ? <AddForm /> : ''}
 
     </div>
   )
